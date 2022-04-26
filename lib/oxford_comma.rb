34c4cd9cdbbff612cd81df1b044e7b5b7966d83c
Lizzie <lizziewrab@me.com>
2022-04-26 17:03:27 -0500

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array[0] + " and " + array[1]
+    else 
+        array_minus_last = array[0...-1]
+        array_minus_last.join(', ') + ", and #{array[-1]}"
+    end
 end
